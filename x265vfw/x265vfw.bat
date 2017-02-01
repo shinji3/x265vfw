3dc1c0a7f281c171b008b7d4c938c77c0080234d
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-02-01 15:44:01 +0900

    インストール用のbatファイルを追加

---
+++ x265vfw/x265vfw.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+SET FILENAME=x265vfw.inf
+TITLE x265vfw - H.265/MPEG-H codec
+ECHO Installing...
+
+REM Workaround for the lost current dir while using "Run as Administrator"
+SETLOCAL ENABLEEXTENSIONS
+IF EXIST "%~dp0" cd /D "%~dp0"
+
+IF NOT EXIST %FILENAME% GOTO FILE_MISSING
+
+IF "%PROCESSOR_ARCHITECTURE%"=="" GOTO INSTALL_X86
+IF %PROCESSOR_ARCHITECTURE%==x86 GOTO INSTALL_X86
+
+"%SystemRoot%\SysWOW64\rundll32.exe" setupapi.dll,InstallHinfSection DefaultInstall 132 .\%FILENAME%
+GOTO INSTALL_DONE
+
+:INSTALL_X86
+rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 132 .\%FILENAME%
+
+:INSTALL_DONE
+CLS
+TYPE ..\COPYING.txt | more
+GOTO End
+
+:FILE_MISSING
+ECHO The file %FILENAME% is missing!
+pause > nul
+
+:End
